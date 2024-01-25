2a437bf637a26eb47bb6d96c643d1a4ac7b54d0c
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-25 23:27:58 +0100

    More code on function debian_install

--- py_script.py
+++ py_script.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 import platform
 import shutil
+import subprocess
 
-if "debian" in platform.version():
+def debian_install():
+    print("Installing on Debian...")
+    print("Updating packages...")
+    subprocess.run(["sudo", "apt-get", "update"])
+    subprocess.run(["sudo", "apt-get", "upgrade"])
+
+def ubuntu_install():
+    pass
+
+distribution = platform.linux_distribution()[0].lower()
+
+if "debian" in distribution:
     print("This is a Debian-based system.")
     debian_install()
-elif "ubuntu" in platform.version():
-    print("This is a Ubuntu-based system.")
+elif "ubuntu" in distribution:
+    print("This is an Ubuntu-based system.")
     ubuntu_install()
 else:
     print("This script is intended for Debian & Ubuntu based systems. Exiting.")
     exit(1)
-
-def debian_install():
-    pass
-def ubuntu_install():
-    pass
