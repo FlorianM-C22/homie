dc25ebe2f66ad00e160f7c3e46249a10b6932122
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-27 17:22:48 +0100

    More changes to py_script to see if I can execute it remotely

--- py_script.py
+++ py_script.py
@@ -2,32 +2,48 @@
 import platform
 import shutil
 import subprocess
+import sys
+
+def check_privileges():
+    if not subprocess.run(["sudo", "-n", "true"]).returncode == 0:
+        print("Error: Insufficient privileges. Please run the script with sudo.")
+        sys.exit(1)
 
 def debian_install():
     print("Installing on Debian...")
+
     print("Updating packages...")
     subprocess.run(["sudo", "apt-get", "update"])
-    subprocess.run(["sudo", "apt-get", "upgrade"])
+
+    print("Upgrading packages...")
+    subprocess.run(["sudo", "apt-get", "upgrade", "-y"])
 
     print("Installing Docker...")
     install_process = subprocess.run(["sudo", "apt-get", "install", "-y", "docker.io"])
-    subprocess.run(["sudo", "apt-get", "install", "-y", "docker.io"])
+
     if install_process.returncode == 0:
-            print("Docker installation completed!")
+        print("Docker installation completed!")
     else:
         print("Error: Docker installation failed.")
 
 def ubuntu_install():
+    print("Installing on Ubuntu...")  # Add Ubuntu-specific installation steps here
     pass
 
-distribution = platform.linux_distribution()[0].lower()
+def main():
+    check_privileges()
 
-if "debian" in distribution:
-    print("This is a Debian-based system.")
-    debian_install()
-elif "ubuntu" in distribution:
-    print("This is an Ubuntu-based system.")
-    ubuntu_install()
-else:
-    print("This script is intended for Debian & Ubuntu based systems. Exiting.")
-    exit(1)
+    distribution = platform.linux_distribution()[0].lower()
+
+    if "debian" in distribution:
+        print("This is a Debian-based system.")
+        debian_install()
+    elif "ubuntu" in distribution:
+        print("This is an Ubuntu-based system.")
+        ubuntu_install()
+    else:
+        print("This script is intended for Debian & Ubuntu based systems. Exiting.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
