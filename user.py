9a3203fe790bec1654c90c8746f480c9469f8f79
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-22 15:50:53 +0100

    More testing on user.py. Was working locally

--- user.py
+++ user.py
@@ -4,17 +4,42 @@ Homie User test module.
 """
 
 import subprocess
+import pwd
+
+
+def user_exists(username):
+    try:
+        pwd.getpwnam(username)
+        return True
+    except KeyError:
+        return False
 
 
 def create_user(username):
-    result = subprocess.run(['sudo', 'useradd', '-m', username],
-                            capture_output=True, text=True)
-    if result.returncode == 0:
-        print(f"User {username} created successfully.")
+    if not user_exists(username):
+        result = subprocess.run(['useradd', '-m', username], capture_output=True, text=True)
+        if result.returncode == 0:
+            print(f"User {username} created successfully.")
+        else:
+            print(f"Failed to create user {username}. Error: {result.stderr}")
     else:
-        print(f"Failed to create user {username}. Error: {result.stderr}")
+        print(f"User {username} already exists.")
+
+
+def create_directory(username, directory):
+    result = subprocess.run(['mkdir', '-p', directory], capture_output=True, text=True)
+    if result.returncode == 0:
+        print(f"Directory {directory} created successfully.")
+    else:
+        print(f"Failed to create directory {directory}. Error: {result.stderr}")
 
 
 def main():
-    create_user('homie')
-    subprocess.run(['sudo', '-u', 'homie', 'mkdir', 'test'], check=True)
+    username = 'homie'
+    directory = 'test'
+    create_user(username)
+    create_directory(username, directory)
+
+
+if __name__ == "__main__":
+    main()
