c72c6dc2d7fbe88a9beff29d0d66b629074d8ec1
FlorianM-C22 <7484@holbertonstudents.com>
2024-06-11 10:31:44 +0200

    Added placeholder to do more tests on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,50 +3,51 @@ import * as React from "react";
 import HomieLogo from './HomieLogo.jsx';
 import WavyBackgroundDemo from "@/components/wavybackground";
 import BoxRevealDemo from "@/components/boxreveal";
+import Image from "next/image";
 
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 
 export default function Home() {
   return (
-      <>
-          <Navbar>
-            <NavbarBrand>
-            <HomieLogo />
-            </NavbarBrand>
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-              <NavbarItem>
-                <Link color="foreground" href="#">
-                  Features
-                </Link>
-              </NavbarItem>
-              <NavbarItem isActive>
-                <Link href="#" aria-current="page">
-                  Documentation
-                </Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Link color="foreground" href="#">
-                  Integrations
-                </Link>
-              </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-              <NavbarItem className="hidden lg:flex">
-                <Link href="/login">Login</Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Button as={Link} color="primary" href="/signup" variant="solid">
-                  Sign Up
-                </Button>
-              </NavbarItem>
-            </NavbarContent>
-          </Navbar>
-          <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
-            <WavyBackgroundDemo />
-          </div>
-          <div className="flex flex-col items-center justify-center h-screen">
-            <BoxRevealDemo />
-          </div>
-      </>
-    );
+    <>
+      <Navbar>
+        <NavbarBrand>
+          <HomieLogo />
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Features
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link href="#" aria-current="page">
+              Documentation
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Integrations
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link href="/login">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" href="/signup" variant="solid">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
+        <WavyBackgroundDemo />
+      </div>
+      <div className="Hero" style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', height: '100vh', padding: '150px' }}>
+        <BoxRevealDemo />
+      </div>
+    </>
+  );
 }
